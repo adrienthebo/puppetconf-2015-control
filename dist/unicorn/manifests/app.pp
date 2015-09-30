7b36e344a183d69154be34cd6d503cdb47fd98a8
Adrien Thebo <git@somethingsinistral.net>
2015-09-30 14:11:01 -0700

    manage unicorn init script

--- dist/unicorn/manifests/app.pp
+++ dist/unicorn/manifests/app.pp
@@ -8,7 +8,9 @@ define unicorn::app(
 
   require "unicorn"
 
-  file { "${app_root}/unicorn_config.rb":
+  $unicorn_config =  "${app_root}/unicorn_config.rb"
+
+  file { $unicorn_config:
     ensure  => present,
     content => template($config_template),
     owner   => $user,
@@ -21,5 +23,26 @@ define unicorn::app(
     owner   => $user,
     group   => $group,
     mode    => "0700",
+  }
+
+  file { "/etc/init/unicorn_${name}.conf":
+    ensure  => present,
+    content => template("unicorn/unicorn-upstart.erb"),
+    owner   => "root",
+    group   => "root",
+    mode    => "0644",
+    notify  => Exec["unicorn service ${name}: refresh upstart"],
+  }
+
+  exec { "unicorn service ${name}: refresh upstart":
+    command     => "/sbin/initctl reload-configuration",
+    refreshonly => true,
+    notify      => Service["unicorn_${name}"],
+  }
+
+  service { "unicorn_${name}":
+    ensure   => running,
+    provider => "upstart",
+    enable   => true,
   }
 }
