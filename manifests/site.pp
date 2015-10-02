d5a85c2157d7d3abbb851e52f4578c14962e8245
Adrien Thebo <git@somethingsinistral.net>
2015-10-02 10:21:38 -0700

    extract  grafana profile

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,27 +43,7 @@ node default {
 
 node /mon1/ {
   include profile::monitoring::graphite
-
-  class { "grafana":
-    install_method => "repo",
-    cfg => {
-      "app_mode" => 'production',
-      "server"   => {
-        "http_port"     => 3000,
-      },
-      database   => {
-        "type"     => 'sqlite3',
-        "host"     => '127.0.0.1:3306',
-        "name"     => 'grafana',
-        "user"     => 'root',
-        "password" => '',
-        "path"     => "grafana.db",
-      },
-      "users"    => {
-        "allow_sign_up" => false,
-      },
-    }
-  }
+  include profile::monitoring::grafana
 }
 
 node /app/ {
